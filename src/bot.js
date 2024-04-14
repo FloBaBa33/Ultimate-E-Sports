115d2806420c484623ecd208f906e82daf3f5dbb
FloBaBa33 <florian_baumbach@hotmail.de>
2024-04-14 13:52:14 +0200

    fixxing the title

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,7 @@ const { Client, IntentsBitField, EmbedBuilder } = require ( "discord.js" )
 const client = new Client ({
     intents: [
         IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.Guilds
     ]
 })
 
@@ -19,6 +20,6 @@ client.on ( "guildMemberAdd", async ( member ) => {
     const welcomeChannel = await member.guild.channels.fetch ( "1221406495300784178" )
     const welcomeEmbed = new EmbedBuilder ()
     .setTitle ( `${ member.guild.name }` )
-    .setDescription ( `Willkommen ${ member }\nVielen DAnk, dass du dich für uns entschieden hast` )
+    .setDescription ( `Willkommen ${ member }\nVielen Dank, dass du dich für uns entschieden hast` )
     await welcomeChannel.send ({ embeds: [ welcomeEmbed ]})
 })
